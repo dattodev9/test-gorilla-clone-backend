4fc6d274b4cdd50a118a2410eb0ba27592d0cf76
Nguyen Thanh Dat <nthdat09@gmail.com>
2025-04-28 21:36:46 +0700

    refactor: enable cors origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import * as bodyParser from 'body-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: [`${process.env.CLIENT_HOST}`],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
