eea937eb18465f2658e283b0e48a13db2427382b
Nguyen Thanh Dat <nthdat09@gmail.com>
2025-04-30 02:17:56 +0700

    feat: for production

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,37 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import 'reflect-metadata';
 import * as bodyParser from 'body-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
   app.enableCors({
-    origin: [`${process.env.CLIENT_HOST}`],
+    origin: configService.get<string>('CLIENT_HOST'),
     credentials: true,
   });
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
   app.use(cookieParser());
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-  const port = process.env.PORT || 8081;
-  await app.listen(port);
-  console.log('Running on port: ', port);
+
+  const port = configService.get<number>('PORT') || 8081;
+
+  if (process.env.NODE_ENV === 'production') {
+    app.enableShutdownHooks();
+    app.useGlobalPipes(
+      new ValidationPipe({ transform: true, whitelist: true }),
+    );
+
+    app.useLogger(['error', 'warn']);
+  }
+
+  await app.listen(port, '0.0.0.0');
+  console.log(`Server running on port: ${port}`);
 }
 
 bootstrap();
